d3002755b077c94d2d82f66818a9c657389681a7
MG92 <migueldam20142015@gmail.com>
2015-01-16 10:27:11 +0100

    .

--- colecciones1/src/diapositivas/arrays.java
+++ colecciones1/src/diapositivas/arrays.java
@@ -4,27 +4,28 @@ public class arrays {
 	public static void main(String[] args) {
 		
 		//int[][] valores=new int[4][3];
-		//
+		//Declaramos un array bidimensional de 4*3
 		int [][] valores= {
 			{14,24,52},
 			{4,86,51},
 			{723,81,319},
 			{4320,1121,120}	
 		};
+		//Creamos 2 variables para hacer la media
+		int suma=0;
+		int numeromedia=0;
+		//Mostramos todos los valores
+		//Valores.length=4
+		//Valores[0].length=3
+		//Despues de mostrar le sumamos los valores en suma y en numeromedia para mostrar el 
+		//total de valores 4*3=12
 		for (int i = 0; i < valores.length; i++) {
 			for (int c = 0; c < valores[0].length; c++) {
 				System.out.print(valores[i][c]+"\t");
-			}
-			System.out.println("\n");
-		}
-	
-		int suma=0;
-		int numeromedia=0;
-		for (int i = 0; i < valores.length; i++) {
-			for (int c = 0; c < valores[0].length; c++) {
 				suma+=valores[i][c];
 				numeromedia++;
 			}
+			System.out.println("\n");
 		}
 		System.out.println("La media es "+ suma/numeromedia);
 	}
