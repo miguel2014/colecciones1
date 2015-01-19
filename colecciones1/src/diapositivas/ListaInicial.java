7b0f2f417df32502fb6c04c279100e0a34771c0e
MG92 <migueldam20142015@gmail.com>
2015-01-19 12:08:56 +0100

    a

---
+++ colecciones1/src/diapositivas/ListaInicial.java
@@ -0,0 +1,13 @@
+package diapositivas;
+
+import java.util.ArrayList;
+
+public class ListaInicial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList list=new ArrayList();
+		System.out.println("Tamaño de la lista: "+list.size());
+	}
+
+}
--- colecciones1/src/diapositivas/arrays.java
+++ colecciones1/src/diapositivas/arrays.java
@@ -1,5 +1,7 @@
 package diapositivas;
 
+import javax.print.attribute.standard.NumberOfInterveningJobs;
+
 public class arrays {
 	public static void main(String[] args) {
 		
@@ -25,8 +27,84 @@ public class arrays {
 				suma+=valores[i][c];
 				numeromedia++;
 			}
-			System.out.println("\n");
+			System.out.println();
 		}
-		System.out.println("La media es "+ suma/numeromedia);
+		
+		System.out.println("\nLa media es "+ suma/numeromedia+"\n");
+		//Definimos un nuevo array con los valores invertidos
+		/*
+		 * i=3
+		 * valores.length=4 en memoria se almacena 0,1,2,3
+		 * valores.length-1= 4-1 =3
+		 * Metemos contadorcolumnas dentro de for porque si no cuando termine con la primera fila
+		 * no se vaciara para ello lo metemos en el bucle
+		 * 
+		 * Como funciona:
+		 * contadorfilas=0;
+		 * contadorcolumnas=0;
+		 * i=3
+		 * c=2
+		 * valores[i][c]=120
+		 * En la primera posicion de nuevosvalores nos pone 120
+		 * nuevosValores[contadorfilas][contadorcolumnas]=valores[i][c]
+		 * nuevosValores[0][0]=valores[3][2]
+		 * 
+		 * Nuevo valor:
+		 * 
+		 * contadorfilas=0;
+		 * contadorcolumnas=1;
+		 * i=3
+		 * c=1
+		 * valores[i][c]=120
+		 * En la segunda posicion de nuevosvalores nos pone 1121
+		 * nuevosValores[contadorfilas][contadorcolumnas]=valores[i][c]
+		 * nuevosValores[0][1]=valores[3][1]
+		 * 
+		 * Nuevo valor:
+		 * 
+		 * contadorfilas=0;
+		 * contadorcolumnas=2;
+		 * i=3
+		 * c=0
+		 * valores[i][c]=4320
+		 * En la tercera posicion de nuevosvalores nos pone 4320
+		 * nuevosValores[contadorfilas][contadorcolumnas]=valores[i][c]
+		 * nuevosValores[0][2]=valores[3][0]
+		 * 
+		 * Nuevo valor:
+		 * 
+		 * contadorfilas=1;
+		 * contadorcolumnas=0;
+		 * i=2
+		 * c=2
+		 * valores[i][c]=120
+		 * En la primera posicion de la nueva fila nuevosvalores nos pone 319
+		 * nuevosValores[contadorfilas][contadorcolumnas]=valores[i][c]
+		 * nuevosValores[1][0]=valores[2][2]
+		 * .......
+		 * 
+		 * */
+		int contadorfilas=0;
+		int [][] nuevosValores=new int[4][3];
+		
+		for (int i = valores.length-1; i >=0; i--) {
+			int contadorcolumnas=0;
+			for (int c = valores[i].length-1; c>=0; c--) {
+
+				nuevosValores[contadorfilas][contadorcolumnas]=valores[i][c];
+				contadorcolumnas++;
+				
+			}
+			contadorfilas++;
+		}
+		//Lo imprimimos
+		for (int i = 0; i < nuevosValores.length; i++) {
+			for (int c = 0; c < nuevosValores[i].length; c++) {
+				System.out.print(nuevosValores[i][c]+"\t");
+				suma+=nuevosValores[i][c];
+			}
+			System.out.println();
+		}
+		
 	}
 }
